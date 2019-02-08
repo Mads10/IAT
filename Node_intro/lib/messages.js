3ce774a8a9255e5c8265e58b2b05e4ba6ac38b73
mads1997 <abdulm13@aston.ac.uk>
2019-02-08 16:42:40 +0000

    step 1.2

---
+++ Node_intro/lib/messages.js
@@ -0,0 +1,43 @@
+const sanitizeHTML = require('sanitize-html');
+
+module.exports = function(url,callback){
+  const mongoose = require('mongoose');
+  mongoose.connect(url,callback);
+
+  const Message = mongoose.model(
+    'messages',
+    {username:String,text:String}
+  );
+    
+  
+
+  return {
+    create:function(newMessage,callback){
+     var message = new Message(newMessage);
+     message.save(callback);
+      
+    },
+    read:function(id,callback){
+     Message.findById(id,callback);
+      
+    },
+    readUsername:function(username,callback){
+      callback();
+    },
+    readAll:function(callback){
+      callback();
+    },
+    update:function(id,updatedMessage,callback){
+      callback();
+    },
+    delete:function(id,callback){
+      callback();
+    },
+    deleteAll:function(callback){
+      Message.remove({},callback);
+    },
+    disconnect:function(){
+      mongoose.disconnect();
+    }
+  };
+};
